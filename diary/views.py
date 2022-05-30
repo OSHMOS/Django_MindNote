b86d000643a3e9c18cd66d1040769c27dca94139
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-05-30 16:50:38 +0900

    Update forms.py with ModelForm

--- diary/views.py
+++ diary/views.py
@@ -15,15 +15,18 @@ def info(request):
 
 def page_create(request):
     if request.method == "POST":
-        new_page = Page(
-            title=request.POST["title"],
-            content=request.POST["content"],
-            feeling=request.POST["feeling"],
-            score=request.POST["score"],
-            dt_created=request.POST["dt_created"],
-        )
-        new_page.save()
-        return redirect("page_detail", page_id=new_page.id)
+        form = PageForm(request.POST)
+        if form.is_valid():
+            new_page = form.save()
+            # new_page = Page(
+            #     title=request.POST["title"],
+            #     content=request.POST["content"],
+            #     feeling=request.POST["feeling"],
+            #     score=request.POST["score"],
+            #     dt_created=request.POST["dt_created"],
+            # )
+            # new_page.save()
+            return redirect("page_detail", page_id=new_page.id)
     else:
         form = PageForm()
         ctx = {"form": form}
