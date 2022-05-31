f81f2eedcc78b1b21dfb320d151262c5cb009981
OSHMOS <87176797+OSHMOS@users.noreply.github.com>
2022-05-31 15:31:14 +0900

    Show anything if we don't have any data

--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Page
 from .forms import PageForm
 
@@ -30,13 +30,13 @@ def page_create(request):
 
 
 def page_detail(request, page_id):
-    object = Page.objects.get(id=page_id)
+    object = get_object_or_404(Page, id=page_id)
     ctx = {"object": object}
     return render(request, "diary/page_detail.html", ctx)
 
 
 def page_update(request, page_id):
-    object = Page.objects.get(id=page_id)
+    object = get_object_or_404(Page, id=page_id)
     if request.method == "POST":
         form = PageForm(request.POST, instance=object)
         if form.is_valid():
@@ -49,7 +49,7 @@ def page_update(request, page_id):
 
 
 def page_delete(request, page_id):
-    object = Page.objects.get(id=page_id)
+    object = get_object_or_404(Page, id=page_id)
     if request.method == "POST":
         object.delete()
         return redirect("page_list")
